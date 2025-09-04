b4eb8460b3cecf4d7ae3c374bd3e3ec6075be657
INSEONG LEE <inseong101@naver.com>
2025-09-04 23:57:32 +0900

    Update script.js

--- scoreboard/script.js
+++ scoreboard/script.js
@@ -20,3 +20,23 @@ window.addEventListener("load", ()=>{
     p.setAttribute("data-open","0");
   });
 });
+function toggleAcc(el){
+  const icon = el.querySelector(".rotate");
+  const panel = el.parentElement.querySelector(".panel");
+  const open = panel.getAttribute("data-open") === "1";
+  if(open){
+    panel.style.maxHeight = "0px";
+    panel.setAttribute("data-open","0");
+    icon && icon.classList.remove("open");
+  }else{
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.setAttribute("data-open","1");
+    icon && icon.classList.add("open");
+  }
+}
+window.addEventListener("load", ()=>{
+  document.querySelectorAll(".panel").forEach(p=>{
+    p.style.maxHeight = "0px";
+    p.setAttribute("data-open","0");
+  });
+});
