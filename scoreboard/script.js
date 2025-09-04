6b7387cbde89f253c751ec68cb8a8011256d4cd2
INSEONG LEE <inseong101@naver.com>
2025-09-04 23:52:56 +0900

    Add files via upload

---
+++ scoreboard/script.js
@@ -0,0 +1,22 @@
+
+function fmtPct(n){ return (n*100).toFixed(1) + "%"; }
+function toggleAcc(el){
+  const ic = el.querySelector(".rotate");
+  const panel = el.parentElement.querySelector(".panel");
+  const open = panel.getAttribute("data-open") === "1";
+  if(open){
+    panel.style.maxHeight = "0px";
+    panel.setAttribute("data-open","0");
+    ic.classList.remove("open");
+  }else{
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.setAttribute("data-open","1");
+    ic.classList.add("open");
+  }
+}
+window.addEventListener("load", ()=>{
+  document.querySelectorAll(".panel").forEach(p => {
+    p.style.maxHeight = "0px";
+    p.setAttribute("data-open","0");
+  });
+});
