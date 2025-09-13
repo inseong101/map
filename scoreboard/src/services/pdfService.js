44185828c832b7e393adc39c35dfcba53dffd277
INSEONG LEE <inseong101@naver.com>
2025-09-13 13:36:24 +0900

    Create pdfService.js

---
+++ scoreboard/src/services/pdfService.js
@@ -0,0 +1,20 @@
+// src/services/pdfService.js
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+export async function fetchWatermarkedPdfBase64(filePath, sid) {
+  const functions = getFunctions();
+  const serve = httpsCallable(functions, "serveWatermarkedPdf");
+  const res = await serve({ filePath, sid });
+  return res.data; // base64
+}
+
+export async function logPdfAction({ filePath, sid, action, meta = {} }) {
+  const functions = getFunctions();
+  const logger = httpsCallable(functions, "logPdfAction");
+  try {
+    await logger({ filePath, sid, action, meta });
+  } catch (e) {
+    // 로깅 실패는 UI 막지 않음
+    console.warn("logPdfAction failed", e);
+  }
+}
