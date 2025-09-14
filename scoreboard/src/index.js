8157c009f7e1ee9c6204b7b8472cafeae67720bd
INSEONG LEE <inseong101@naver.com>
2025-09-14 13:02:52 +0900

    Update index.js

--- scoreboard/src/index.js
+++ scoreboard/src/index.js
@@ -1,4 +1,4 @@
-// src/index.js
+// src/index.js  (서비스워커 캐시 간섭 방지용 추가)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,3 +9,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// ✅ PWA SW가 예전 pdf.js/자원 캐싱해 둔 경우 “차단/빈 화면” 유발 방지
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations?.().then(regs => {
+    regs.forEach(r => r.unregister());
+  });
+}
