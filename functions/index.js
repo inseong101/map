b6e508535b431f44fa656cae6b735bec5815dcef
INSEONG LEE <inseong101@naver.com>
2025-09-24 08:54:27 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-// functions/index.js
+
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { PDFDocument, rgb, degrees, StandardFonts } = require("pdf-lib");
